796eea702a31ca931d8e85f4b13f131949c0a7b8
Georgiana Mania <georgiana.mania@desy.de>
2022-06-09 14:10:40 +0200

    add runtime validations for chain

--- backend/all/include/vecpar/all/chain_orchestrator.hpp
+++ backend/all/include/vecpar/all/chain_orchestrator.hpp
@@ -2,10 +2,19 @@
 #define VECPAR_CHAIN_HPP
 
 #include <functional>
+#include <cassert>
 
 #include <vecmem/memory/host_memory_resource.hpp>
 #include <vecmem/containers/vector.hpp>
 #include "../../extern/composition/Compose.hpp"
+
+#define MISSING_ALGORITHMS\
+    "A chain should have at least one algorithm. Please call with_algorithms() function."
+
+#define ALGORITHMS_ALREADY_SET \
+    "A list of algorithms was already provided. If more algorithms are needed, enqueue them in the previous with_algorithms() call."
+
+#define assertm(exp, msg) assert(((void)msg, exp))
 
 namespace vecpar {
 
@@ -32,11 +41,19 @@ namespace vecpar {
 
         template<typename... Args>
         chain_orchestrator& with_algorithms(Args... args) {
+            /// cannot call with_algorithms more than once
+            assertm(!algorithms_set, ALGORITHMS_ALREADY_SET);
+
             composition = compose(wrapper(args)...);
+            algorithms_set = true;
+
             return *this;
         }
 
         R execute(H& coll, OtherInput... rest) {
+            /// cannot invoke chain execution without providing algorithms
+            assertm(algorithms_set, MISSING_ALGORITHMS);
+
             return composition(coll, rest...);
         }
 
@@ -85,6 +102,7 @@ namespace vecpar {
         MemoryResource& m_mr;
         vecpar::config m_config;
         std::function<R(H&, OtherInput...)> composition;
+        bool algorithms_set = false;
     };
 }
 #endif //VECPAR_CHAIN_HPP
