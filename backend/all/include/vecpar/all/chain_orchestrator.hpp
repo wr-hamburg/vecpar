4ac3536f4dfa72adcf4fbaa6cb29588b923b37de
Georgiana Mania <georgiana.mania@desy.de>
2022-06-09 11:10:09 +0200

    link composition api as external module

--- backend/all/include/vecpar/all/chain_orchestrator.hpp
+++ backend/all/include/vecpar/all/chain_orchestrator.hpp
@@ -1,46 +1,11 @@
 #ifndef VECPAR_CHAIN_HPP
 #define VECPAR_CHAIN_HPP
 
-#include <utility>
 #include <functional>
 
 #include <vecmem/memory/host_memory_resource.hpp>
 #include <vecmem/containers/vector.hpp>
-
-//TODO: extract this in a dependency module
-namespace external {
-#define INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE\
-    "An object should be invokable with the provided arguments"
-
-    template<typename F>
-    auto compose(F &&f) {
-        return [a = std::forward<F>(f)](auto &&... args)  {
-            static_assert(std::is_invocable_v<F, decltype(args)...>,
-                          INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE);
-            return a(std::forward<decltype(args)>(args)...);
-        };
-    }
-
-    template<typename F1, typename F2, typename... Fs>
-    auto compose(F1 &&f1, F2 &&f2, Fs &&... fs) {
-        return compose(
-                [first = std::forward<F1>(f1), second = std::forward<F2>(f2)]
-                        (auto &&... args)  {
-                    static_assert(std::is_invocable_v<F1, decltype(args)...>,
-                                  INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE
-                    );
-                    static_assert(
-                            std::is_invocable_v<F2,
-                                    std::result_of_t<F1(decltype(args)...)>
-                            >,
-                            INVOCABLE_ASSERTION_FAILED_ERROR_MESSAGE
-                    );
-                    return second(first(std::forward<decltype(args)>(args)...));
-                },
-                std::forward<Fs>(fs)...
-        );
-    }
-}
+#include "../../extern/composition/Compose.hpp"
 
 namespace vecpar {
 
@@ -67,7 +32,7 @@ namespace vecpar {
 
         template<typename... Args>
         chain_orchestrator& with_algorithms(Args... args) {
-            composition = external::compose(wrapper(args)...);
+            composition = compose(wrapper(args)...);
             return *this;
         }
 
@@ -76,7 +41,8 @@ namespace vecpar {
         }
 
     private:
-   /*
+
+        /*
         template <class Algorithm,
                 class Input = typename Algorithm::input_type,
                 class Output = typename Algorithm::output_type_t,
@@ -84,7 +50,7 @@ namespace vecpar {
         auto wrapper(Algorithm& algorithm) {
 
 #if defined(__CUDA__) && defined(__clang__)
-                //
+            //
 #else
             return [&](Input& coll, OtherInput... otherInput) -> Output& {
                 if (first_fn) {
@@ -97,15 +63,12 @@ namespace vecpar {
             };
 #endif
         }
-
-   typename std::enable_if_t<!std::is_base_of<vecmem::host_memory_resource, MemoryResource>::value, bool> = true>
-
-        */
-
+*/
         //TODO: see if wrapper duplication can be avoided
         template <class Algorithm,
                 class Input = typename Algorithm::input_type,
-                class Output = typename Algorithm::output_type_t>
+                class Output = typename Algorithm::output_type_t>//,
+   //             typename std::enable_if_t<!std::is_base_of<vecmem::host_memory_resource, MemoryResource>::value, bool> = true>
         auto wrapper(Algorithm& algorithm) {
             return [&](Input& coll, OtherInput... otherInput) -> Output& {
                 if (first_fn) {
